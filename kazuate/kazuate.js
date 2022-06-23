3c9f0470114d6f0a40c49ba2a7ab559c7fb314a3
G084962020 <208496@st.takushoku-u.ac.jp>
2022-06-23 14:51:49 +0900

    0623

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -4,7 +4,7 @@ console.log('答え: ' + kotae);      // デバッグ用
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
+let s =0;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
@@ -16,6 +16,22 @@ hantei();
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
+    kaisu++;
+    console.log(kaisu+"回目の予想："+yoso);
+
+    if(kotae === yoso){
+        console.log("正解です。おめでとう！");
+    }
+     else  if(kaisu >= 4){
+        console.log("答えは"+kotae+"でした．すでにゲームは終わっています");
+}
+    else if(kotae > yoso){
+        console.log("まちがい。答えはもっと大きいですよ");
+    }
+    else if(kotae < yoso){
+        console.log("まちがい。答えはもっと小さいですよ");
+    }
+
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 
