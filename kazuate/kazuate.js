9c3e234ef234cf2b3f79501d6103aba7c0365ddd
208496 <208496@st.takushoku-u.ac.jp>
2022-07-16 20:59:51 +0900

    0710

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -9,50 +9,51 @@ let s =0;
 
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
-hantei();
-hantei();
-hantei();
-
-let presult = document.querySelector('p#result');
-span = document.createElement('span');
-presult.insertAdjacentElement('afterend',span);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    let yoso = 1;       // 第5回課題:テキストボックスの数値をここに代入
+    let b = document.querySelector('#print');
+    b.addEventListener('click', greeting);
+
+// 2. イベントハンドラの定義
+function greeting() {
+	let i = document.querySelector('input[name="seisuu"]');
+	let seisuu = i.value;
+	
+
+    let yoso = seisuu;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     kaisu++;
     let presult = document.querySelector('p#result');
     span = document.createElement('span');
     presult.insertAdjacentElement('afterend',span);
 
-    span.textContent = (kaisu+"回目の予想："+yoso);
 
-    if(kotae === yoso){
-        let presult = document.querySelector('p#result');
-        span = document.createElement('span');
-        span.textContent = ("正解です。おめでとう！");
+    span.textContent = (kaisu+"回目の予想："+yoso+"  ");
+
+    let presult2 = document.querySelector('p#result2');
+    li = document.createElement('li');
+    presult2.insertAdjacentElement('afterend',li);
+
+
+
+    if(kotae == yoso){
+        li.textContent = ("正解です。おめでとう！");
         
     }
-     else  if(kaisu >= 4){
-        let presult = document.querySelector('p#result');
-        span = document.createElement('span');
-        span.textContent = ("答えは"+kotae+"でした．すでにゲームは終わっています");
+     if(kaisu >= 4){
+        li.textContent = ("答えは"+kotae+"でした．すでにゲームは終わっています");
         
 }
     else if(kotae > yoso){
-        let presult = document.querySelector('p#result');
-        span = document.createElement('span');
-        span.textContent =("まちがい。答えはもっと大きいですよ");
+        li.textContent =("まちがい。答えはもっと大きいですよ");
         
     }
     else if(kotae < yoso){
-        let presult = document.querySelector('p#result');
-        span = document.createElement('span');
-        span.textContent =("まちがい。答えはもっと小さいですよ");
+        li.textContent =("まちがい。答えはもっと小さいですよ");
         
     }
-
+}
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 }
